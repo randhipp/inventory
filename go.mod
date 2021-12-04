43bc867740936ad0272ac582d57fdea5425696fc
Randhi Pratama Putra <randhi.pp@gmail.com>
2021-12-04 11:37:14 +0700

    add gorm and test connection to db

--- go.mod
+++ go.mod
@@ -11,4 +11,16 @@ require (
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	gorm.io/gorm v1.22.4
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
+	gorm.io/driver/mysql v1.2.1
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0
 )
--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/randhipp/inventory/database"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+func initDatabase() {
+	var err error
+	dsn := fmt.Sprintf("%v:%v@%v/%v?multiStatements=true&parseTime=true",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOSTNAME"),
+		os.Getenv("DB_NAME"),
+	)
+	database.DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	app := fiber.New()
+	initDatabase()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
-
 	app.Listen(":3000")
 }
